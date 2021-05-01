3784685d9535e9b50f458797e2a49422c47032fe
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-04-30 20:47:39 -0400

    setup Express server at port 8080 and setup proxy

--- server/server.js
+++ server/server.js
@@ -1,16 +1,65 @@
 const express = require('express');
-const path = require('path');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
+/**
+ * require routers
+ */
+// const userRouter = require('./routes/users');
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+/**
+ * handle parsing request body
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * Parse Cookie header and populate req.cookies with an object keyed by the cookie names. Optionally you may enable signed cookie support by passing a secret string, which assigns req.secret so it may be used by other middleware.
+ *
+ */
+app.use(cookieParser());
+
+/**
+ * define route handlers
+ */
+
+// app.use('/user', userRouter);
+
+/**
+ * catch-all route handler for any requests to an unknown route
+ */
+
+app.use('*', (req, res) => {
+  return res.sendStatus(404);
 });
 
-app.get('/ping', function (req, res) {
-  return res.json('pong');
+/**
+ * configure express global error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log('errorObj.message --->', errorObj.message);
+  console.log('errorObj.status --->', errorObj.status);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// eslint-disable-next-line no-unused-vars
+
+/**
+ * start server
+ */
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
