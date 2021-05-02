6fde0ad47527f80e8d0801d67dc4b60e6cf65722
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-05-01 20:01:07 -0400

    updated react router in App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Signin from './components/Signin/Signin';
 import Signup from './components/Signup/Signup';
-import Products from './components/Products/Products';
-import Detail from './components/Detail/Detail';
+import ProductList from './components/ProductList/ProductList';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 import MainNav from './components/MainNav/MainNav';
 
 // import './App.css';
 
 function App() {
-	return (
-		<div className='App'>
-			<Router>
-				<MainNav />
-				<Switch>
-					<Route exact path='/signin'>
-						<Signin />
-					</Route>
+  return (
+    <div className="App">
+      <Router>
+        <MainNav />
+        <Switch>
+          <Route exact path="/signin">
+            <Signin />
+          </Route>
 
-					<Route exact path='/signup'>
-						<Signup />
-					</Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
 
-					<Route exact path='/'>
-						<Products />
-					</Route>
+          <Route exact path="/">
+            <ProductList />
+          </Route>
 
-					<Route exact path='/products/:id'>
-						<Detail />
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
+          <Route exact path="/products/:id">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
