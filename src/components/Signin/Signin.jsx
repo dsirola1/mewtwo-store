0306d1d738e6514963f9528f9212fb27208afb8a
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-05-01 22:28:28 -0400

    completed Signin Component

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,5 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default function Signin() {
-  return <div>This is the Signin Component</div>;
-}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Signin = () => {
+  const classes = useStyles();
+  // react hooks
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleSignin = async () => {
+    try {
+      const res = await axios.post('/user/signin', { email, password });
+      console.log('Signed In!');
+      console.log('res.data ---> ', res.data);
+      history.push('/');
+    } catch (error) {
+      if (error.response.status === 401) {
+        history.push('/signin');
+      }
+      console.log(
+        'Error in handleSubmit in Signin component: ',
+        error.response.data.err
+      );
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSignin();
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            label="Email Address"
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            label="Password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Typography align="center">
+            <Link href="/signup" variant="body2">
+              Don't have an account? Sign Up
+            </Link>
+          </Typography>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          Copyright © mewtwo-store {new Date().getFullYear()}.
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Signin;
