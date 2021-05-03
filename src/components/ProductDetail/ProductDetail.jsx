83202ef96fea033c03ed8fbe3a9ecf34728c3319
AndreaLi <andrea.li8341@hotmail.com>
2021-05-02 19:25:56 -0700

    update

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 import useProduct from './_useProduct';
+import CheckoutButton from '../Stripe-Checkout/StripeCheckout';
 
 import './ProductDetail.css';
 
@@ -9,60 +10,61 @@ import './ProductDetail.css';
 // fetch data based on id for product
 
 const tempStyle = {
-  margin: '0 auto',
-  maxWidth: '1200px',
-  padding: '30px',
+	margin: '0 auto',
+	maxWidth: '1200px',
+	padding: '30px',
 };
 
 function ProductDetail() {
-  //get id from router
-  const { id } = useParams();
+	//get id from router
+	const { id } = useParams();
 
-  function handleClick() {
-    console.log('ADD TO CART', id);
-  }
+	function handleClick() {
+		console.log('ADD TO CART', id);
+	}
 
-  const [loading, product] = useProduct(id);
+	const [loading, product] = useProduct(id);
 
-  const title = loading ? (
-    <h1 className="mockTitle"></h1>
-  ) : (
-    <h1 className="productTitle">{product.title}</h1>
-  );
-  const description = loading ? (
-    <p className="mockDescription"></p>
-  ) : (
-    <p className="productDescription">{product.description}</p>
-  );
-  const productPrice = loading ? '$' : `$${product.price}`;
+	const title = loading ? (
+		<h1 className='mockTitle'></h1>
+	) : (
+		<h1 className='productTitle'>{product.title}</h1>
+	);
+	const description = loading ? (
+		<p className='mockDescription'></p>
+	) : (
+		<p className='productDescription'>{product.description}</p>
+	);
+	const productPrice = loading ? '$' : `$${product.price}`;
 
-  return (
-    <div style={tempStyle}>
-      <div className="breadCrumbWrapper">
-        <Link to="/">&larr; Back to all products</Link>
-      </div>
-      <div className="productContainer">
-        <div className="productImage">
-          <div className="productImageWrapper">
-            <img
-              src={loading ? '' : product.image}
-              alt={product ? product.title : ''}
-            ></img>
-          </div>
-        </div>
-        <div className="productInfo">
-          {title}
-          <div className="priceWrapper">
-            <span>{productPrice}</span>
-          </div>
-          <button className="productButton" onClick={handleClick}>
-            Add to Cart
-          </button>
-          {description}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div style={tempStyle}>
+			<div className='breadCrumbWrapper'>
+				<Link to='/'>&larr; Back to all products</Link>
+			</div>
+			<div className='productContainer'>
+				<div className='productImage'>
+					<div className='productImageWrapper'>
+						<img
+							src={loading ? '' : product.image}
+							alt={product ? product.title : ''}
+						></img>
+					</div>
+				</div>
+				<div className='productInfo'>
+					{title}
+					<div className='priceWrapper'>
+						<span>{productPrice}</span>
+					</div>
+					<button className='productButton' onClick={handleClick}>
+						Add to Cart
+						<CheckoutButton />
+					</button>
+					{description}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProductDetail;
