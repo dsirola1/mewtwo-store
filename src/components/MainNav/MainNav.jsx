ea50fcce03845abe8c8c6ae13ac1cff5433e649a
AndreaLi <andrea.li8341@hotmail.com>
2021-05-04 12:07:05 -0700

    styling navBar

--- src/components/MainNav/MainNav.jsx
+++ src/components/MainNav/MainNav.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import Signout from '../MainNav/Signout';
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
+
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
@@ -10,45 +11,94 @@ import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import Badge from '@material-ui/core/Badge';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles((theme) => ({
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+		},
+	},
+	sectionDesktop: {
+		display: 'none',
+		[theme.breakpoints.up('md')]: {
+			display: 'flex',
+		},
+	},
+	sectionMobile: {
+		display: 'flex',
+		[theme.breakpoints.up('md')]: {
+			display: 'none',
+		},
+	},
+}));
 
 export default function MainNav() {
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+	const handleMobileMenuOpen = (event) => {
+		setMobileMoreAnchorEl(event.currentTarget);
+	};
+	const mobileMenuId = 'primary-search-account-menu-mobile';
+
 	return (
-		<Box>
+		<div>
 			<AppBar position='static'>
 				<Toolbar>
-					<Link exact to='/'>
-						<Typography variant='h6' noWrap component='div'>
-							MEWTWO
-						</Typography>
-					</Link>
-
-					<Box>
+					<Typography variant='h6' noWrap component='div'>
 						<Link exact to='/'>
-							<IconButton color='inherit'>
-								<FormatListBulletedIcon />
-							</IconButton>
+							MEWTWO
 						</Link>
+					</Typography>
 
-						<Link to='/checkout'>
-							<IconButton color='inherit'>
+					<div className={classes.grow} />
+					<div className={classes.sectionDesktop}>
+						<IconButton color='inherit'>
+							<Link exact to='/'>
+								<FormatListBulletedIcon />
+							</Link>
+						</IconButton>
+
+						<IconButton color='inherit'>
+							<Link to='/checkout'>
 								<Badge badgeContent={4} color='secondary'>
 									<ShoppingCartIcon />
 								</Badge>
-							</IconButton>
-						</Link>
+							</Link>
+						</IconButton>
 
-						<Link exact to='/account'>
-							<IconButton color='inherit'>
+						<IconButton color='inherit'>
+							<Link exact to='/account'>
 								<AccountCircle />
-							</IconButton>
-						</Link>
+							</Link>
+						</IconButton>
 
 						<Link exact to='/signout'>
 							<Signout />
 						</Link>
-					</Box>
+					</div>
+					<div className={classes.sectionMobile}>
+						<IconButton
+							aria-label='show more'
+							aria-controls={mobileMenuId}
+							aria-haspopup='true'
+							onClick={handleMobileMenuOpen}
+							color='inherit'
+						>
+							<MoreIcon />
+						</IconButton>
+					</div>
 				</Toolbar>
 			</AppBar>
-		</Box>
+		</div>
 	);
 }
