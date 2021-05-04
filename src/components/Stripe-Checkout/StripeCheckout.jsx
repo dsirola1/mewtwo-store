825dc0f47c1bc95866680a9bf582a8ee302bc40b
AndreaLi <andrea.li8341@hotmail.com>
2021-05-03 17:03:53 -0700

    add store icon

--- src/components/Stripe-Checkout/StripeCheckout.jsx
+++ src/components/Stripe-Checkout/StripeCheckout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import logo from '../../assets/Mewtwo.png';
 
 export default function CheckoutButton() {
 	const pushlishableKey =
 		'pk_test_51Imi4SIAnLFPMvKfMUV8zcYvy6D6iOMpX6IN62VrIsQob9bqyZ0sK9sT1kzbXhiUA6JYfg5DE9s92CAaPuXBmVKq00cWn7LFMx';
 
 	const onToken = (token) => {
-		console.log(token);
+		console.log('token', token);
+		alert('payment successful');
 	};
 	return (
 		<StripeCheckout
@@ -17,6 +19,7 @@ export default function CheckoutButton() {
 			billingAddress
 			zipCode
 			panelLabel='Pay Now'
+			image={logo}
 			description='Your total is $20'
 			amount={2000000}
 		/>
