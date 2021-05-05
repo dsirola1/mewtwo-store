18acb4fc00246af871e94d407929081fcd02b70e
AndreaLi <andrea.li8341@hotmail.com>
2021-05-04 18:01:26 -0700

    finish responsive dropdown in navBar

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import { ProvideAuth } from './routes/useAuth';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ProvideAuth>
-        <App />
-      </ProvideAuth>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<ProvideAuth>
+			<App />
+		</ProvideAuth>
+	</BrowserRouter>,
+
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
