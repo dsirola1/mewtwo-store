64c8473983c76d22918482f3f4982c2ac35f116e
dsirola1 <63817459+dsirola1@users.noreply.github.com>
2021-05-05 23:57:45 -0700

    styling for search input added

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './ProductList.css';
 import ProductSingle from './ProductListSingle';
-// import SearchBar from './SearchBar'
 
 export default function ProductList() {
   const [list, setList] = useState([]);
@@ -12,42 +11,42 @@ export default function ProductList() {
 	const loadData = async () => {
 		const response = await fetch('https://fakestoreapi.com/products');
 		const data = await response.json();
-    setList(data);
+		setList(data);
+		// console.log(data)
 	};
 
-  return (
-    <div className="eachProd">
-      <input
-        value = {searchInput}
-        className="searchTextBox"
-        type="text"
-        placeholder="Search by Name"
-        onChange={(e) => setSearchInput(e.target.value)}
-      ></input>
-      <button 
-        className="searchButton" 
-        onClick={() => console.log('search button clicked')}
-        >Search
-      </button>
-      {list.filter((val) => {
-        if (searchInput == '') {
-          return val;
-        } else if (val.title.toLowerCase().includes(searchInput.toLowerCase())) {
-          return val;
-        }
-      }).map(({ id, title, image, price, description }) => {
-        // console.log(title)
-        return (
-          <ProductSingle
-            key={id}
-            id={id}
-            title={title}
-            image={image}
-            price={price}
-            description={description}
-          />
-        );
-      })}
-    </div>
-  );
+	const product = list.filter((val) => {
+    if (searchInput == '') {
+      return val;
+    } else if (val.title.toLowerCase().includes(searchInput.toLowerCase())) {
+      return val;
+    }
+  }).map(({ id, title, image, price, description }) => {
+		// console.log(title)
+		return (
+			<ProductSingle
+				key={id}
+				id={id}
+				title={title}
+				image={image}
+				price={price}
+				description={description}
+			/>
+		);
+	});
+	return (
+		<div className='eachProd'>
+			<div>
+				{/* <label htmlFor="search">Search by Name</label> */}
+				<input
+					className='searchTextBox'
+					type='text'
+          placeholder='Search by Name'
+          onChange={(e) => setSearchInput(e.target.value)}
+				></input>
+				{/* <button className='searchButton'>Search</button> */}
+			</div>
+			<div className='product'>{product}</div>
+		</div>
+	);
 }
