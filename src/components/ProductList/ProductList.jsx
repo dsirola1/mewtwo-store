7f2f106741e6eaa4646ed8cd5774d2bc26e03283
dsirola1 <63817459+dsirola1@users.noreply.github.com>
2021-05-05 20:01:23 -0700

    got search bar working

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,32 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './ProductList.css';
 import ProductSingle from './ProductListSingle';
-import SearchBar from './SearchBar'
+// import SearchBar from './SearchBar'
 
 export default function ProductList() {
-	const [list, setList] = useState([]);
+  const [list, setList] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
 	useEffect(() => {
 		loadData();
 	}, []);
 	const loadData = async () => {
 		const response = await fetch('https://fakestoreapi.com/products');
 		const data = await response.json();
-		setList(data);
-		// console.log(data)
+    setList(data);
 	};
 
   return (
     <div className="eachProd">
-      {/* <div>
-        <input
-          className="searchTextBox"
-          type="text"
-          placeholder="Search by Name"
-        ></input>
-        <button className="searchButton">Search</button>
-      </div> */}
-      <SearchBar></SearchBar>
-      {list.map(({ id, title, image, price, description }) => {
+      <input
+        value = {searchInput}
+        className="searchTextBox"
+        type="text"
+        placeholder="Search by Name"
+        onChange={(e) => setSearchInput(e.target.value)}
+      ></input>
+      <button 
+        className="searchButton" 
+        onClick={() => console.log('search button clicked')}
+        >Search
+      </button>
+      {list.filter((val) => {
+        if (searchInput == '') {
+          return val;
+        } else if (val.title.toLowerCase().includes(searchInput.toLowerCase())) {
+          return val;
+        }
+      }).map(({ id, title, image, price, description }) => {
         // console.log(title)
         return (
           <ProductSingle
