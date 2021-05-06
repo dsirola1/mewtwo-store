e5ac8f271e4f3e2ec74c1adfd18700e9008e632a
AndreaLi <andrea.li8341@hotmail.com>
2021-05-06 16:32:08 -0700

    product page styling and update

--- src/components/ProductList/ProductListSingle.jsx
+++ src/components/ProductList/ProductListSingle.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const ProductListSingle = ({ id, title, price, image }) => {
-  return (
-    <Link className='productLink' exact to={`/products/${id}`}>
-      <div className={'prodSingle'}>
-        <h2>{title}</h2>
-        <h2>Price: ${price}</h2>
-        <img className={'imgProd'} src={image} alt={title} />
-        <h3>Free Two Day Delivery with Mewtwo Store Prime</h3>
-      </div>
-    </Link>
-  );
+	return (
+		<Link className='productLink' exact to={`/products/${id}`}>
+			<div className='prodSingle'>
+				<h2>{title}</h2>
+
+				<img className='imgProd' src={image} alt={title} />
+				{/* <h3>Free Two Day Delivery with Mewtwo Store Prime</h3> */}
+				<h3>${price}</h3>
+			</div>
+		</Link>
+	);
 };
 export default ProductListSingle;
